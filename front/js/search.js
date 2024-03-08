f91cae7e12578bb3acc7f1a7d11bbc7317b0e147
Thomas Marques <thomas.marques@edu.devinci.fr>
2024-03-08 10:38:18 +0100

    ✨ UPDATE search conditions

--- front/js/search.js
+++ front/js/search.js
@@ -2,6 +2,10 @@ const searchBar = document.querySelector('input[name="search"]')
 
 searchBar.addEventListener('keyup', async () => {
     const searchValue = searchBar.value
+
+    if(searchValue.length < 3 && searchValue.length !== 0) {
+        return
+    }
     const tolearnsList = document.querySelector('.tolearns__list');
     const tolearnsListElements = tolearnsList.querySelectorAll('.tolearns__list__item')
     const todosList = document.querySelector('.todos__list');
